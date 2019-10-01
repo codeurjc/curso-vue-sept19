2e052bcaad5d43b80c56ae3d90551f570869711b
micaelgallego <micael.gallego@gmail.com>
2019-10-01 12:54:47 +0200

    Add ejers 1 and 1b

---
+++ ejer1b/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="input">
+    <button @click="addElem()">Añadir</button>
+    <ul>
+      <li v-for="(elem,i) in elems" :key="elem">
+        {{elem}}
+        <button @click="removeElem(i)">Borrar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data: () => {
+    return {
+      input: "",
+      elems: []
+    }
+  },
+  methods:{
+    addElem: function(){
+      this.elems.push(this.input);
+      this.input = "";
+    },
+    removeElem: function(i){
+      this.elems.splice(i,1);
+    }
+  }
+};
+</script>
